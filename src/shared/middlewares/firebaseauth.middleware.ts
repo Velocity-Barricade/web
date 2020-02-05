d2ae252bcd71deefe2593dfe1674ad192723f224
Saad Ismail <saad3112@gmail.com>
2020-02-05 20:00:23 +0500

    use firebaseUid to authorize instead of jwtToken

--- src/shared/middlewares/firebaseauth.middleware.ts
+++ src/shared/middlewares/firebaseauth.middleware.ts
@@ -1,23 +1,22 @@
-import * as jwt from 'jsonwebtoken';
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { MessageCodeError } from '../errors/message-code-error';
-import { FirebaseUser } from '../../modules/firebase-user/firebase-user.entity';
 import * as admin from 'firebase-admin';
 
 @Injectable()
 export class FirebaseAuthMiddleware implements NestMiddleware {
     public async use(req, res, next) {
         if (req.headers.authorization) {
-            const idToken = (req.headers.authorization as string);
+            const firebaseUid = (req.headers.authorization as string);
 
-            // Verify firebase user here using firebase-admin
-            admin.auth().verifyIdToken(idToken).then(decodedToken => {
-                req.body["firebase_uid"] = decodedToken.uid;
-                req.body["firebase_email"] = decodedToken.email;
+            // Get firebase user from firebase using uid
+            try {
+                let firebaseUser = await admin.auth().getUser(firebaseUid);
+                req.body["firebase_uid"] = firebaseUser.uid;
+                req.body["firebase_email"] = firebaseUser.email;
                 next();
-            }).catch(error => {
+            } catch {
                 throw new MessageCodeError('firebase:cantdecode');
-            });
+            }
         } else {
             throw new MessageCodeError('request:unauthorized');
         }
